00889c9adadb727c1e3f8b7b27d1d6bbe0b332fa
ElysiumCodes <shaktivel2k4@gmail.com>
2025-02-16 17:47:05 +0530

    added servo control

---
+++ servoControl.ino
@@ -0,0 +1,21 @@
+#include <ESP32Servo.h>
+#define SERVO_PIN1 13  
+#define SERVO_PIN2 14
+
+Servo serv1;  // Create Servo object
+Servo serv2;
+
+void setup() {
+    serv1.attach(SERVO_PIN1);
+    serv2.attach(SERVO_PIN2);
+}
+
+void loop() {
+  int i,j;
+  for(i=0;i<=180;i+=5){
+    for(j=0;j<=180;j+=5){
+      serv2.write(j);
+    }
+    serv1.write(i);
+  }
+}
